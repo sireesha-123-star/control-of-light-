79c8f6d91722a73bf7ddcef9e6aed16247dae2f6
sireesha-123-star <73226709+sireesha-123-star@users.noreply.github.com>
2020-10-22 15:02:34 +0530

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -10,12 +10,12 @@ aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
   
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
 import os
-def turnoff(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned off")
+def off(update, context):
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Light is turning off")
   context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_12676342_filament-lamp-on-a-white-background-illustration-for-design.html')
   send_value(0)
-def turnon(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned on")
+def on(update, context):
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Light is turning on")
   context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_48123160_stock-vector-bright-glowing-incandescent-light-bulb-on-a-white-background.html')
   send_value(1)
 
@@ -25,28 +25,28 @@ def send_value(value):
 
 def input_message(update, context):
   text=update.message.text
-  if text == 'turn on':
+  if text == 'on':
     send_value(1)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned on")
+    context.bot.send_message(chat_id=update.effective_chat.id,text="Light is turning on")
     context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_48123160_stock-vector-bright-glowing-incandescent-light-bulb-on-a-white-background.html')
-  elif text == 'turn off':
+  elif text == 'off':
     send_value(0)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned off")
+    context.bot.send_message(chat_id=update.effective_chat.id,text="Light is turning off")
     context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://www.123rf.com/photo_12676342_filament-lamp-on-a-white-background-illustration-for-design.html')
 
     
 def start(update,context):
   start_message='''
-/turnoff :To turn OFF the Light
-/turnon :To turn ON the Light
+/off :To turn OFF the Light
+/on :To turn ON the Light
 '''
   context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
 
 
 updater =Updater('1399937407:AAHNUzeL8RGKXYe2NabUEB7Ah1fDhhqt7J0')
 dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('turnoff',turnoff))
-dispatcher.add_handler(CommandHandler('turnon',turnon))
+dispatcher.add_handler(CommandHandler('off',off))
+dispatcher.add_handler(CommandHandler('on',on))
 dispatcher.add_handler(CommandHandler('start',start))
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command),input_message))
 updater.start_polling
